6e57631c12d370451d7d34f84b9e017c3492e406
matthewlamanque <matthewrejeanlamanque@gmail.com>
2020-05-03 21:30:32 -0400

    Initial code or documentation commit. No versioning, yet.

---
+++ back-end/userOps/cryptography-EDP-userOps.js
@@ -0,0 +1,13 @@
+/**
+* Project Cryptography
+* Use Operations API (Back-end)
+* Author:
+* Owner: 
+*/
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send("Hello, World!"))
+
+app.listen(port, () => console.log("Listening on port: 3000"))
